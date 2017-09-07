a0c5b6393a178e8af7c168ceae06542c9338cc16
Charles BOCHET <charlesb@theodo.fr>
2017-09-07 13:11:38 +0200

    Use native like navigation

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import logo from '../logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import MenuItem from 'material-ui/MenuItem';
+import Drawer from 'material-ui/Drawer';
 
 class Page extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {menuOpen: false};
+    }
+
+    handleToggle = () => this.setState({menuOpen: !this.state.menuOpen});
+
     render() {
-        return (<div className="App">
-            <div className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>{this.props.title}</h2>
+        return (<MuiThemeProvider>
+            <div className="App">
+                <AppBar
+                    title="Cropchat"
+                    onLeftIconButtonTouchTap={this.handleToggle}
+                />
+                <Drawer
+                    open={this.state.menuOpen}
+                    docked={false}
+                    onRequestChange={(menuOpen) => this.setState({menuOpen})}
+                >
+                    <MenuItem
+                        onClick={this.handleToggle}
+                        containerElement={<Link to="/" />}
+                        primaryText="Home"
+                    />
+                    <MenuItem
+                        onClick={this.handleToggle}
+                        containerElement={<Link to="/post" />}
+                        primaryText="Post"
+                    />
+                </Drawer>
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>{this.props.title}</h2>
+                </div>
+                <p className="App-intro">
+                    This is the {this.props.title} page.
+                </p>
             </div>
-            <p className="App-intro">
-                This is the {this.props.title} page.
-            </p>
-            <p>
-                <Link to="/">Home</Link>
-            </p>
-            <p>
-                <Link to="/post">Post</Link>
-            </p>
-        </div>);
+        </MuiThemeProvider>);
     }
 }
 
